863fe08e6fea98c722b308a20d2cca8e78068709
pinnockm2 <pinnockm2@coventry.ac.uk>
2022-01-13 16:53:16 +0000

    Added neccesary libaries and added a wallet config

--- cli/index.ts
+++ cli/index.ts
@@ -1,4 +1,8 @@
-import web3 = require("@solana/web3.js")
+
+import { Keypair, LAMPORTS_PER_SOL, sendAndConfirmTransaction, SystemProgram, Transaction } from '@solana/web3.js';
+import * as web3 from '@solana/web3.js';
+import base58 from 'bs58';
+import config from '../shared/config';
 async function main(){
 
 }
