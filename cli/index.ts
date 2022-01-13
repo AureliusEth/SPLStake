d951bdb5ff8cd884f89a6999af884225a4ce50a2
pinnockm2 <pinnockm2@coventry.ac.uk>
2022-01-13 02:12:16 +0000

    Intial CLI env

---
+++ cli/index.ts
@@ -0,0 +1,10 @@
+import web3 = require("@solana/web3.js")
+async function main(){
+
+}
+main()
+    .then(() => process.exit)
+    .catch((error)=> {
+        console.error(error);
+        process.exit(1)
+    })
