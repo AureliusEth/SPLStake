9ae2f691d3accdecde5d89c7dc1c88dca48dc617
pinnockm2 <pinnockm2@coventry.ac.uk>
2022-01-14 23:59:48 +0000

    Added program data and compiled and deployed code

--- cli/index.ts
+++ cli/index.ts
@@ -4,19 +4,20 @@ import * as web3 from '@solana/web3.js';
 import base58 from 'bs58';
 import config from '../shared/config';
 const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
-const programId  = new web3.PublicKey("FthrfCyLNJW3i7sPto5tyju8upYvVWyRzTTp8cUDkfeK")
+const programId  = new web3.PublicKey("3NAsw1q2XpgqNj4tpQUBvZ1PmwupLnfStvDdTiB9MYV7")
 const key: Uint8Array = Uint8Array.from([9,107,84,74,163,67,202,96,121,49,136,163,149,4,146,171,41,152,210,46,128,23,70,150,25,78,218,127,7,176,133,62,221,66,111,57,66,6,198,70,240,89,153,84,130,3,123,16,8,86,9,124,0,144,71,210,47,240,85,245,118,5,236,100])
 
 async function main(){
     const signer: web3.Keypair=web3.Keypair.fromSecretKey(key)
-    await connection.getBalance(signer.publicKey).then((balance) => {
+    await  connection.getBalance(signer.publicKey).then((balance) => {
         console.log("SOL: ", balance / web3.LAMPORTS_PER_SOL);
+        
     });
-
+    const data: Buffer = Buffer.from(Uint8Array.of(1)) 
     const transaction = new web3.Transaction().add(new web3.TransactionInstruction({
         keys: [],
-        programId: programId,
-
+        programId,
+        data,
     }));
     await web3.sendAndConfirmTransaction(connection, transaction , [signer]).then((sig) =>{
         console.log("sig: {}", sig)
