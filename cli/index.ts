9caf6d4084335e39d8c362e4e64bb0664299edf6
pinnockm2 <pinnockm2@coventry.ac.uk>
2022-01-16 16:43:23 +0000

    gettinf file ready for merge with test

--- cli/index.ts
+++ cli/index.ts
@@ -6,27 +6,7 @@ const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
 const programId  = new web3.PublicKey("895ZeWDVGjZmoSwbPq72Ms3q7QxkL2bN83EAn1LVcGay")
 const key: Uint8Array = Uint8Array.from([9,107,84,74,163,67,202,96,121,49,136,163,149,4,146,171,41,152,210,46,128,23,70,150,25,78,218,127,7,176,133,62,221,66,111,57,66,6,198,70,240,89,153,84,130,3,123,16,8,86,9,124,0,144,71,210,47,240,85,245,118,5,236,100])
 const senderKeypair: Keypair = Keypair.generate();
-async function main(){
-    var _balance = 0;
-    const signer: web3.Keypair=web3.Keypair.fromSecretKey(key)
-    await  connection.getBalance(signer.publicKey).then((balance) => {
-        console.log("SOL: ", balance / web3.LAMPORTS_PER_SOL);
-        
-    });
-    const data: Buffer = Buffer.from(Uint8Array.of(1)) 
-    const transaction = new web3.Transaction().add(new web3.TransactionInstruction({
-        keys: [],
-        programId,
-        data,
 
-    }));
-    await web3.sendAndConfirmTransaction(connection, transaction , [signer]).then((sig) =>{
-        console.log("sig: {}", sig)
-    })
-     
-    const Arrived = await connection.requestAirdrop(senderKeypair.publicKey, 1000000000);
-    await connection.confirmTransaction(Arrived);    
-}
 var create_reward_pool
 setTimeout(async function(){ 
     
@@ -57,11 +37,3 @@ setTimeout(async function(){
 }, 20000);
 
 
-
-main()
-    .then(() => process.exit)
-    .catch((error)=> {
-        console.error(error);
-        process.exit(1)
-
-    })
